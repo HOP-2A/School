34ccbb69cc6685bc0a5250475079243c2de95d92
Amin-Erdene <amin-erdene@admins-iMac.local>
2025-12-25 15:16:06 +0800

    toaster

--- app/admin/classes/[classId]/page.tsx
+++ app/admin/classes/[classId]/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { useParams } from "next/navigation";
 import { ChangeEvent, useEffect, useState } from "react";
-import toast from "react-hot-toast";
+import { toast } from "sonner";
 
 type ClassType = {
   id: string;
