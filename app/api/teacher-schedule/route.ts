d3505fe2698e5451013135bfecd386dc71a788de
Amin-Erdene <amin-erdene@admins-iMac.local>
2025-12-30 15:35:00 +0800

    teacher-change

--- app/api/teacher-schedule/route.ts
+++ app/api/teacher-schedule/route.ts
@@ -1,32 +1,26 @@
 import { prisma } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-
-export async function POST(req: NextRequest) {  
-    const body = await req.json()
-    const existed = await prisma.teacherSchedule.findFirst({
-      where:{
-        endTime:body.endTime,
-        startTime:body.startTime,
-        day:body.day,
-        teacherId:body.teacherId
-      }
-
-    })
-    if(!existed){
-      const createNewSchedule = await prisma.teacherSchedule.create({
-        data:{
-      endTime:body.endTime,
-      startTime:body.startTime,
-      teacherId:body.teacherId,
-      day:body.day,
-      classId:body.classId
-      
-        }
-        });
-        return NextResponse.json(createNewSchedule, {status:200});
-    }
-else return NextResponse.json("time's are coin",{status:404});
-    
-
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const existed = await prisma.teacherSchedule.findFirst({
+    where: {
+      endTime: body.endTime,
+      startTime: body.startTime,
+      day: body.day,
+      teacherId: body.teacherId,
+    },
+  });
+  if (!existed) {
+    const createNewSchedule = await prisma.teacherSchedule.create({
+      data: {
+        endTime: body.endTime,
+        startTime: body.startTime,
+        teacherId: body.teacherId,
+        day: body.day,
+        classId: body.classId,
+      },
+    });
+    return NextResponse.json(createNewSchedule, { status: 200 });
+  } else return NextResponse.json("time's are coin", { status: 404 });
 }
