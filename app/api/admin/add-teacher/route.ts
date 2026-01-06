a7c4f89b3f0686dd89fd82ea1631997646d6e8f5
Kajusii <kmbqueensky@gmail.com>
2026-01-06 17:21:01 +0800

    change

--- app/api/admin/add-teacher/route.ts
+++ app/api/admin/add-teacher/route.ts
@@ -4,13 +4,22 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const { classId, teacherId } = body;
+const existed  = await prisma.teacherClass.findFirst({
+  where:{
+    teacherId : teacherId,
+    classId: classId,
+  }
+ 
+})
+if(existed){
+  return NextResponse.json("Can't add teacher", { status: 400 });
+}else { await prisma.teacherClass.create({
+  data: {
+    classId: classId,
+    teacherId: teacherId,
+  },
+});
 
-  await prisma.teacherClass.create({
-    data: {
-      classId: classId,
-      teacherId: teacherId,
-    },
-  });
-
-  return NextResponse.json("Success", { status: 200 });
+return NextResponse.json("Success", { status: 200 });
+}
 }
